f18d539f5e1a7c1fef16daf1391a826956d16534
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2025-09-17 12:31:57 -0300

    Criação de um post de produtos.

--- src/index-mongo.ts
+++ src/index-mongo.ts
@@ -13,8 +13,19 @@ app.use(express.json())
 //criando uma rota para acesso pelo navegador
 app.get('/produtos', async (req:Request, res:Response) => {
     const produtos = await db.collection('produtos').find().toArray()
-    res.json(produtos)
+    res.status(200).json(produtos)
 })
+
+app.post("/produtos", async (req:Request, res:Response) => {
+    const {nome,preco,urlfoto,descricao} = req.body
+    if(!nome || !preco || !urlfoto || !descricao)
+        return res.status(400).json({error:"Nome, preço, urlfoto e descrição são obrigatórios"})
+
+    const produto = { nome, preco, urlfoto, descricao }
+    const resultado = await db.collection('produtos').insertOne(produto)
+    res.status(201).json({nome,preco,urlfoto,descricao,_id:resultado.insertedId})
+})
+
 // Criando o servidor na porta 8000 com o express
 app.listen(8000, () => {
     console.log('Server is running on port 8000')
