4cb32b5a22528fe966669fe87f98cd4a980e6e0a
Vânia Moreira <vania.santos3@estudante.ifms.edu.br>
2025-09-29 08:31:00 -0400

    Atualização atividade

--- src/produtos/produtos.controller.ts
+++ src/produtos/produtos.controller.ts
@@ -1,19 +1,19 @@
-import { Request, Response } from 'express'
-import { db } from '../database/banco-mongo.js'
-class ProdutosController {
-    async adicionar(req: Request, res: Response) {
-        const { nome, preco, urlfoto, descricao } = req.body
-        if (!nome || !preco || !urlfoto || !descricao)
-            return res.status(400).json({ error: "Nome, preço, urlfoto e descrição são obrigatórios" })
-
-        const produto = { nome, preco, urlfoto, descricao }
-        const resultado = await db.collection('produtos').insertOne(produto)
-        res.status(201).json({ nome, preco, urlfoto, descricao, _id: resultado.insertedId })
+import { Request, Response } from "express";
+import { db } from "../database/banco-mongo.js";
+class ProdutoController{
+    async adicionar(req:Request, res:Response){
+        const {nome,preco,urlfoto,descricao} = req.body
+     if(!nome || !preco || !urlfoto || !descricao) {
+        return res.status(400).json({error: 'Nome, preço, urlfoto e descrição são obrigatórios'})
     }
-    async listar(req: Request, res: Response) {
-        const produtos = await db.collection('produtos').find().toArray()
-        res.status(200).json(produtos)
+    const produto = {nome,preco,urlfoto,descricao}
+    const resultado = await db.collection('produtos').insertOne(produto)
+    res.status(201).json({nome,preco,urlfoto,descricao,_id: resultado.insertedId})
+    }
+    async listar(req:Request, res:Response){
+         const produtos = await db.collection('produtos').find().toArray()
+         res.json(produtos)
     }
 }
 
-export default new ProdutosController()
+export default new ProdutoController()
