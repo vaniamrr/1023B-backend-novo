000327f18d9f667a3483c3e3f8e53ba97bd05aaf
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2025-10-20 12:54:05 -0300

    Alteração do tempo do token para 1h

--- src/usuarios/usuarios.controller.ts
+++ src/usuarios/usuarios.controller.ts
@@ -38,7 +38,7 @@ class UsuariosController {
         if(!senhaValida) return res.status(401).json({mensagem:"Senha Incorreta!"})
 
         //Gerar o token
-        const token = jwt.sign({usuarioId: usuario._id}, process.env.JWT_SECRET!, {expiresIn: '1m'})
+        const token = jwt.sign({usuarioId: usuario._id}, process.env.JWT_SECRET!, {expiresIn: '1h'})
         res.status(200).json({token:token})
     }
 }
